b9b6ba82530626b91b1f9ee33843f5a8692d1e59
Caleb Champlin <caleb.champlin@gmail.com>
2026-01-12 07:14:03 +0000

    feat(component): implement decoder preamble parsing

---
+++ internal/component/binary/decoder_test.go
@@ -0,0 +1,68 @@
+// internal/component/binary/decoder_test.go
+
+package binary
+
+import (
+	"testing"
+
+	"github.com/tetratelabs/wazero/internal/testing/require"
+)
+
+func TestDecodeComponent_Preamble(t *testing.T) {
+	tests := []struct {
+		name        string
+		input       []byte
+		expectedErr error
+	}{
+		{
+			name:        "empty",
+			input:       []byte{},
+			expectedErr: ErrInvalidMagic,
+		},
+		{
+			name:        "magic only",
+			input:       Magic[:],
+			expectedErr: ErrUnexpectedEOF,
+		},
+		{
+			name:        "wrong magic",
+			input:       []byte{0x00, 0x00, 0x00, 0x00, 0x0d, 0x00, 0x01, 0x00},
+			expectedErr: ErrInvalidMagic,
+		},
+		{
+			name:        "wrong version",
+			input:       append(Magic[:], 0x01, 0x00, 0x00, 0x00, 0x01, 0x00),
+			expectedErr: ErrInvalidVersion,
+		},
+		{
+			name:        "core module layer",
+			input:       append(append(Magic[:], Version[:]...), LayerModule[:]...),
+			expectedErr: ErrInvalidLayer,
+		},
+		{
+			name:        "valid empty component",
+			input:       append(append(Magic[:], Version[:]...), LayerComponent[:]...),
+			expectedErr: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		tc := tt
+		t.Run(tc.name, func(t *testing.T) {
+			_, err := DecodeComponent(tc.input)
+			if tc.expectedErr != nil {
+				require.Equal(t, tc.expectedErr, err)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}
+
+func TestDecodeComponent_EmptyComponent(t *testing.T) {
+	input := append(append(Magic[:], Version[:]...), LayerComponent[:]...)
+
+	c, err := DecodeComponent(input)
+	require.NoError(t, err)
+	require.NotNil(t, c)
+}
